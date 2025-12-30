64ec1b9f2c415a48e8718548a17e118d236c9af9
NANASANA IRFAN <139986589+irfan21381@users.noreply.github.com>
2025-12-30 13:10:22 -0500

    Update EditCourse.tsx

--- frontend/src/pages/admin/EditCourse.tsx
+++ frontend/src/pages/admin/EditCourse.tsx
@@ -1,1 +1,36 @@
- 
+ import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CourseForm from "@/admin/components/courses/CourseForm";
+
+export default function EditCourse() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    title: "React Fundamentals",
+    description: "Learn React step by step",
+    category: "Web Development",
+    level: "Beginner",
+    status: "Published",
+  });
+
+  const handleChange = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log("UPDATED COURSE:", form);
+    navigate("/admin/courses");
+  };
+
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-2xl font-bold">Edit Course</h1>
+
+      <CourseForm
+        form={form}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  );
+}
